12761c6011b8b6c96ca8667da0ac3ec0f5f44308
Gabe <Carrollgbusiness@gmail.com>
2026-04-21 16:07:40 -0500

    Formatted Date

--- src/main/java/com/pluralsight/DisplayTime.java
+++ src/main/java/com/pluralsight/DisplayTime.java
@@ -1,6 +1,8 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DisplayTime {
     public static void main(String[] args) {
@@ -8,10 +10,20 @@ public class DisplayTime {
         /*04/22/2026
         2026-04-22
         Wednesday 22, 2026
-        Wednesday, Apr 22, 2026 10:02 <---- display in GMT
-        5:02 on 22-Apr-2026 <---- display in CST
+        Wednesday, Apr 22, 2026 10:02 <---- display in GMT Time
+        5:02 on 22-Apr-2026 <---- display in cst Time
          */
-        LocalDateTime today = LocalDateTime.now();
+        LocalDate today = LocalDate.now();
         System.out.println(today);
+
+
+        DateTimeFormatter formatter1  = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDate = today.format(formatter1);
+        System.out.println(formattedDate);
+
+
+
+
+
     }
 }
