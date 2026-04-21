60d8bfdc8c5ed768d49a82a781dfd4de57ab8c35
Gabe <Carrollgbusiness@gmail.com>
2026-04-21 15:27:30 -0500

    First commit

--- src/main/java/com/pluralsight/DisplayTime.java
+++ src/main/java/com/pluralsight/DisplayTime.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+
 public class DisplayTime {
+    public static void main(String[] args) {
+        //display current time in following formats
+        /*04/22/2026
+        2026-04-22
+        Wednesday 22, 2026
+        Wednesday, Apr 22, 2026 10:02 <---- display in GMT
+        5:02 on 22-Apr-2026 <---- display in CST
+         */
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println(today);
+    }
 }
